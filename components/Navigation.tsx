96a2f5b1ed5c27b2f3c8e5cb74ec5001aef0efe8
Rki0 <pky00823@gmail.com>
2023-11-20 12:07:00 +0900

    refactor: create Link tag dynamically in Navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,16 +3,53 @@ import Link from "next/link";
 interface NavigationProps {
   nav_style: string;
   list_style: string;
+  onClickHandler: () => void;
 }
+
+interface Menu {
+  id: number;
+  path: string;
+  text: string;
+}
+
+// TODO: implement condition to show element after id 4 to only logged-in user
+const NAVIGATION_MENU: Menu[] = [
+  {
+    id: 1,
+    path: "/map",
+    text: "지도",
+  },
+  {
+    id: 2,
+    path: "/community",
+    text: "커뮤니티",
+  },
+  {
+    id: 3,
+    path: "/mypage",
+    text: "마이 페이지",
+  },
+  {
+    id: 4,
+    path: "/login",
+    text: "로그인/회원가입",
+  },
+  {
+    id: 5,
+    path: "/feed",
+    text: "글쓰기",
+  },
+];
 
 const Navigation = (props: NavigationProps) => {
   return (
     <nav className={props.nav_style}>
       <ul className={props.list_style}>
-        <Link href="/map">지도</Link>
-        <Link href="/community">커뮤니티</Link>
-        <Link href="/mypage">마이 페이지</Link>
-        <Link href="/login">로그인/회원가입</Link>
+        {NAVIGATION_MENU.map((menu) => (
+          <Link key={menu.id} href={menu.path} onClick={props.onClickHandler}>
+            {menu.text}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
