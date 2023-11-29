aeaed0c5b89c3e5835fb5467011a9a76830aaf03
Rki0 <pky00823@gmail.com>
2023-11-29 19:15:59 +0900

    refactor: handle onClick event from outside of component

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 interface ButtonProps {
   type: "button" | "submit" | "reset";
-  children: string;
+  children: any;
+  onClickHandler?: () => void;
 }
 
 function Button(props: ButtonProps) {
-  return <button type={props.type}>{props.children}</button>;
+  return (
+    <button type={props.type} onClick={props.onClickHandler}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
