474ca4bcf0a711854bc790dc254f52ec662700c0
Rki0 <pky00823@gmail.com>
2023-12-28 11:08:51 +0900

    feat: detect window resizing and close sidemenu automatically

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Header from "./Header/Header";
 import SideMenu from "./SideMenu/SideMenu";
 import SideMenuBackground from "./SideMenu/SideMenuBackground";
 
 import styles from "./Layout.module.scss";
+import useUser from "@/query/useUser";
 
 const Layout = (props: { children: React.ReactNode }) => {
   const [toggleMenu, setToggleMenu] = useState<boolean>(false);
@@ -12,6 +13,39 @@ const Layout = (props: { children: React.ReactNode }) => {
   const toggleMenuHandler = () => {
     setToggleMenu((prev) => !prev);
   };
+
+  const debounce = (func: () => void, wait: number) => {
+    let timeout: NodeJS.Timeout;
+
+    return function executedFunction(this: any) {
+      const later = () => {
+        timeout = null!;
+        func.apply(this);
+      };
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(later, wait);
+    } as any;
+  };
+
+  const detectWindowResize = debounce(() => {
+    if (window.innerWidth <= 1024) {
+      return;
+    }
+
+    setToggleMenu(false);
+  }, 300);
+
+  useEffect(() => {
+    window.addEventListener("resize", detectWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", detectWindowResize);
+    };
+  }, []);
+
+  const user = useUser();
 
   return (
     <div className={styles.layout}>
