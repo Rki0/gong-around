1d9b6845e4a16911bff1005fb69b903268156e1a
Rki0 <pky00823@gmail.com>
2023-12-28 11:07:58 +0900

    style: add CSS to nav-bar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,8 @@
 import Link from "next/link";
+import { useContext } from "react";
+
+import { AuthContext } from "@/context/authContext";
+import useLogOutQuery from "@/query/useLogOutQuery";
 
 interface NavigationProps {
   nav_style: string;
@@ -12,7 +16,6 @@ interface Menu {
   text: string;
 }
 
-// TODO: implement condition to show element after id 4 to only logged-in user
 const NAVIGATION_MENU: Menu[] = [
   {
     id: 1,
@@ -21,36 +24,77 @@ const NAVIGATION_MENU: Menu[] = [
   },
   {
     id: 2,
-    path: "/community",
+    path: "/community?page=1",
     text: "커뮤니티",
   },
   {
     id: 3,
+    path: "/login",
+    text: "로그인/회원가입",
+  },
+];
+
+const LOGGED_IN_NAVIGATION_MENU: Menu[] = [
+  {
+    id: 1,
+    path: "/map",
+    text: "지도",
+  },
+  {
+    id: 2,
+    path: "/community?page=1",
+    text: "커뮤니티",
+  },
+  {
+    id: 3,
+    path: "/feed",
+    text: "글쓰기",
+  },
+  {
+    id: 4,
     path: "/mypage",
     text: "마이 페이지",
   },
   {
-    id: 4,
-    path: "/login",
-    text: "로그인/회원가입",
-  },
-  {
     id: 5,
-    path: "/feed",
-    text: "글쓰기",
+    path: "/logout",
+    text: "로그아웃",
   },
 ];
 
 const Navigation = (props: NavigationProps) => {
+  const authCtx = useContext(AuthContext);
+  const logOutMutation = useLogOutQuery();
+
+  const getNavigationList = (menu: Menu) => {
+    if (menu.path === "/logout") {
+      return (
+        <button type="button" onClick={() => logOutMutation()}>
+          {menu.text}
+        </button>
+      );
+    }
+
+    return (
+      <Link href={menu.path} onClick={props.onClickHandler}>
+        {menu.text}
+      </Link>
+    );
+  };
+
+  const createNavigationMenu = (menuList: Menu[]) => {
+    return menuList.map((menu) => (
+      <li key={menu.id}>{getNavigationList(menu)}</li>
+    ));
+  };
+
+  const MENU = authCtx.isLoggedIn
+    ? createNavigationMenu(LOGGED_IN_NAVIGATION_MENU)
+    : createNavigationMenu(NAVIGATION_MENU);
+
   return (
     <nav className={props.nav_style}>
-      <ul className={props.list_style}>
-        {NAVIGATION_MENU.map((menu) => (
-          <Link key={menu.id} href={menu.path} onClick={props.onClickHandler}>
-            {menu.text}
-          </Link>
-        ))}
-      </ul>
+      <ul className={props.list_style}>{MENU}</ul>
     </nav>
   );
 };
