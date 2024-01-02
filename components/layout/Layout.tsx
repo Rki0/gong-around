7a81f28202bd3a3ae3d227a57685a64a018cb47f
Rki0 <pky00823@gmail.com>
2024-01-02 11:35:06 +0900

    feat: add feature to block scroll when side menu was opened

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -45,6 +45,24 @@ const Layout = (props: { children: React.ReactNode }) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (!toggleMenu) {
+      return;
+    }
+
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, [toggleMenu]);
+
   const user = useUser();
 
   return (
